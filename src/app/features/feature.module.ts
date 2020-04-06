9a709b12976fbbcbfc78caa743ab05ac697f35b4
Siriwasan <siriwasan@gmail.com>
2020-04-06 12:52:20 +0700

    add ScrollSpy module

--- src/app/features/feature.module.ts
+++ src/app/features/feature.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
+import { HomeComponent } from './home/home.component';
+import { TestFormComponent } from './registry/test-form/test-form.component';
+import { LoremIpsumComponent } from './registry/test-form/lorem-ipsum.component';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, TestFormComponent, LoremIpsumComponent],
+  imports: [CoreModule, SharedModule],
 })
-export class FeatureModule { }
+export class FeatureModule {}
