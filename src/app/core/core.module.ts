aece2b04d3511df58e5fb5648a7d5e286449e415
Siriwasan <siriwasan@gmail.com>
2020-04-06 13:05:01 +0700

    bug fixed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MaterialModule } from '../shared/material.module';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [HeaderComponent, NavbarComponent],
@@ -16,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     ReactiveFormsModule,
     MaterialModule,
     FlexLayoutModule,
