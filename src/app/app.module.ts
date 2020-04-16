f8c91afc2973b4dedcc3182aa79d283a08b4fdcc
Siriwasan <siriwasan@gmail.com>
2020-04-16 11:32:44 +0700

    fixed theme with dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import * as Hammer from 'hammerjs';
@@ -16,7 +16,9 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { FeatureModule } from './features/feature.module';
-import { AppStoreModule } from './store/app';
+import { AppStoreModule, AppStoreSelectors } from './store/app';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { AppState } from './store/root-store.state';
 
 // export class MyHammerConfig extends HammerGestureConfig {
 //   overrides = <any>{
@@ -44,4 +46,14 @@ import { AppStoreModule } from './store/app';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private store: Store<AppState>, private overlayContainer: OverlayContainer) {
+    this.store.select(AppStoreSelectors.theme).subscribe((theme) => {
+      if (theme === 'dark') {
+        this.overlayContainer.getContainerElement().classList.add('dark-theme');
+      } else {
+        this.overlayContainer.getContainerElement().classList.remove('dark-theme');
+      }
+    });
+  }
+}
