5fcc0e354ca76c67b25ea1e388bdf47b3ea3c9d5
Siriwasan <siriwasan@gmail.com>
2020-05-03 12:45:05 +0700

    complete Acsd290 sectionM

--- src/app/features/registry/acsd/acsd290/acsd290.component.ts
+++ src/app/features/registry/acsd/acsd290/acsd290.component.ts
@@ -145,7 +145,7 @@ export class Acsd290Component extends RegistryFormComponent
 
     this.createForm();
     this.registryFormService.subscribeFormConditions();
-    this.formGroupI.controls['OpValve'].setValue('Yes');
+    this.formGroupI.controls['OpOCard'].setValue('Yes');
   }
 
   ngAfterViewInit() {
